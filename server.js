ca74b4e18adc951d2c451e89e29c91599b10643b
bcbridges <bcb35b@gmail.com>
2021-08-27 11:25:12 -0600

    Added in fs and util modules.

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const path = require("path");
+// fs to read/write files
 const fs = require("fs");
+// util methods can create promise objs
+const util = require("util");
 
+// Assigns port number to either the Heroku port or local 3001
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Sends user's to the homepage upon open
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
